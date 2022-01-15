427c3bd8fbefa1ddfe9eb2a0ece76a996de149e1
nerdchanii <kimego@knou.ac.kr>
2022-01-15 17:40:00 +0900

    확장자포맷 통일

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Ide from './components/ide/Ide';
 import Topper from './components/Layout/Topper';
 import RouteContainer from './components/Layout/RouteContainer';
 import Footer from './components/Layout/Footer';
