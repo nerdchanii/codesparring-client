04de5bd190e1e282c15fb640d13cea3453a05170
nerdchanii <63002393+nerdchanii@users.noreply.github.com>
2022-01-22 12:31:40 +0900

    Nerdchanii/issue#14 (#21)
    useCallback을 사용하여 fetchData 함수 반복정인 생성 방지

--- src/components/Problem/ProblemList.jsx
+++ src/components/Problem/ProblemList.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
-import { loadding } from '../../state/loading';
-import { useRecoilState } from 'recoil';
+import { useCallback, useEffect, useState } from 'react';
+// import { loadding } from '../../state/loading';
+// import { useRecoilState } from 'recoil';
 import { Link } from 'react-router-dom';
 import './Problem.scss';
 
@@ -30,58 +30,60 @@ const ProblemListItem = ({ problem }) => {
 };
 
 const ProblemList = () => {
-  const [listLoadding, setListLoadding] = useRecoilState(loadding);
+  // const [listLoadding, setListLoadding] = useRecoilState(loadding);
   const [problemItems, setProblemItems] = useState(null);
+  const [loadding, setLoadding] = useState(false);
 
-  useEffect(() => {
-    const fectchData = async () => {
-      setListLoadding(true);
-      try {
-        const response = await Promise.resolve([
-          {
-            id: 1,
-            level: 1,
-            title: '입력된 수 더하기',
-            problemType: '구현',
-          },
-          { id: 2, level: 2, title: '해시브라운해쉬', problemType: '구현' },
-          { id: 3, level: 2, title: '가운데 숫자넣기', problemType: '구현' },
-          { id: 4, level: 3, title: '풀 스택 배낭', problemType: '그리디' },
-          {
-            id: 5,
-            level: 4,
-            title: '어느길로 가지?',
-            problemType: 'A*알고리즘',
-          },
-          { id: 6, level: 5, title: '6번 문제', problemType: '구현' },
-          { id: 7, level: 5, title: '7번문제', problemType: '구현' },
-          { id: 8, level: 2, title: '8번문제', problemType: '구현' },
-          { id: 9, level: 9, title: '9번 문제', problemType: '문제종류' },
-          { id: 10, level: 9, title: '10번 문제', problemType: '문제종류' },
-          { id: 11, level: 9, title: '11번 문제', problemType: '문제종류' },
-          { id: 12, level: 9, title: '12번 문제', problemType: '문제종류' },
-          { id: 13, level: 9, title: '13번 문제', problemType: '문제종류' },
-          { id: 14, level: 9, title: '14번 문제', problemType: '문제종류' },
-          { id: 15, level: 9, title: '15번 문제', problemType: '문제종류' },
-          { id: 16, level: 9, title: '16번 문제', problemType: '문제종류' },
-          { id: 17, level: 9, title: '17번 문제', problemType: '문제종류' },
-          { id: 18, level: 9, title: '18번 문제', problemType: '문제종류' },
-          { id: 19, level: 9, title: '19번 문제', problemType: '문제종류' },
-          { id: 20, level: 9, title: '20번 문제', problemType: '문제종류' },
-          { id: 21, level: 9, title: '21번 문제', problemType: '문제종류' },
-        ]);
-        console.log(response);
-        setProblemItems(response);
-      } catch (e) {
-        console.log(e);
-      } finally {
-        setListLoadding(false);
-      }
-    };
-    fectchData();
+  const fetchData = useCallback(async () => {
+    const response = await Promise.resolve([
+      {
+        id: 1,
+        level: 1,
+        title: '입력된 수 더하기',
+        problemType: '구현',
+      },
+      { id: 2, level: 2, title: '해시브라운해쉬', problemType: '구현' },
+      { id: 3, level: 2, title: '가운데 숫자넣기', problemType: '구현' },
+      { id: 4, level: 3, title: '풀 스택 배낭', problemType: '그리디' },
+      {
+        id: 5,
+        level: 4,
+        title: '어느길로 가지?',
+        problemType: 'A*알고리즘',
+      },
+      { id: 6, level: 5, title: '6번 문제', problemType: '구현' },
+      { id: 7, level: 5, title: '7번문제', problemType: '구현' },
+      { id: 8, level: 2, title: '8번문제', problemType: '구현' },
+      { id: 9, level: 9, title: '9번 문제', problemType: '문제종류' },
+      { id: 10, level: 9, title: '10번 문제', problemType: '문제종류' },
+      { id: 11, level: 9, title: '11번 문제', problemType: '문제종류' },
+      { id: 12, level: 9, title: '12번 문제', problemType: '문제종류' },
+      { id: 13, level: 9, title: '13번 문제', problemType: '문제종류' },
+      { id: 14, level: 9, title: '14번 문제', problemType: '문제종류' },
+      { id: 15, level: 9, title: '15번 문제', problemType: '문제종류' },
+      { id: 16, level: 9, title: '16번 문제', problemType: '문제종류' },
+      { id: 17, level: 9, title: '17번 문제', problemType: '문제종류' },
+      { id: 18, level: 9, title: '18번 문제', problemType: '문제종류' },
+      { id: 19, level: 9, title: '19번 문제', problemType: '문제종류' },
+      { id: 20, level: 9, title: '20번 문제', problemType: '문제종류' },
+      { id: 21, level: 9, title: '21번 문제', problemType: '문제종류' },
+    ]);
+
+    setProblemItems(response);
   }, []);
 
-  if (listLoadding) {
+  useEffect(() => {
+    setLoadding(true);
+    try {
+      fetchData();
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoadding(false);
+    }
+  }, []);
+
+  if (loadding) {
     return <p> ...loadding</p>;
   }
   if (!problemItems) {
