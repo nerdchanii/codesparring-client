20db52b17156468fbacc8123f7d445432affafdd
nerdchanii <kimego@knou.ac.kr>
2022-01-26 15:04:30 +0900

    네아로 추가
    일단 기능만 추가됨, 서버에 인증상태관리는 일정미룸

--- src/state/login/index.js
+++ src/state/login/index.js
@@ -1,7 +1,13 @@
 import { atom } from 'recoil';
 const IS_LOGIN = 'isLogin';
+const NAVER_ACESS_TOKEN = 'naverAccessToken';
 
 export const isLogin = atom({
   key: IS_LOGIN,
   default: false,
+});
+
+export const naverAcessToken = atom({
+  key: NAVER_ACESS_TOKEN,
+  default: null,
 });
