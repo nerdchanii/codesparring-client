4fb18f4fdd1e3949aecd867ad32819430fa233c8
nerdchanii <kimego@knou.ac.kr>
2022-01-29 20:58:19 +0900

    api서버를 위한 Proxy설정

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 import Topper from './components/Layout/Topper';
 import RouteContainer from './components/Layout/RouteContainer';
 import Footer from './components/Layout/Footer';
-import './App.scss'
+import './App.scss';
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Topper />
-      <hr /> 
-      <RouteContainer /> 
-      <hr />
+      <RouteContainer />
       <Footer />
     </div>
   );
