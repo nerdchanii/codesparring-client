6c97512509aba02635fadfc84ceba66cc2ce35d3
nerdchanii <kimego@knou.ac.kr>
2022-02-01 23:43:25 +0900

    소켓io 패키지 추가 및 CORS를 위한 작업( #1)

--- src/setProxy.js
+++ src/setProxy.js
@@ -1,11 +1,25 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+//NOT USE
 
-module.exports = function (app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'http://localhost:5000',
-      changeOrigin: true,
-    }),
-  );
-};
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// module.exports = (app) => {
+//   app.use(
+//     // createProxyMiddleware(['/api', '/socket.io'], {
+//     //   target: 'http://localhost:5000/',
+//     //   changeOrigin: true,
+//     //   router: {
+
+//     //   },
+//     // }),
+//     // createProxyMiddleware('/socket.io', {
+//     //   target: 'http://localhost:5005/',
+//     //   changeOrigin: true,
+//     // }),
+//   );
+//   // // app.use(
+//   //   createProxyMiddleware('/socket.io', {
+//   //     target: 'http://localhost:5050/',
+//   //     changeOrigin: true,
+//   //   }),
+//   // );
+// };
