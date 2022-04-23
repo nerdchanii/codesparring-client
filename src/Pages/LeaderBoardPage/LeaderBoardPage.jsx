28689d42a16c0ba5e32ba20add93bd72f97ad2ff
nerdchanii <nerdchanii@gmail.com>
2022-04-23 19:49:38 +0900

    add lint ignore
    immer library can makes using imutable update

--- src/Pages/LeaderBoardPage/LeaderBoardPage.jsx
+++ src/Pages/LeaderBoardPage/LeaderBoardPage.jsx
@@ -1,25 +1,11 @@
-import axios from 'axios';
-import React, { useEffect } from 'react';
+import React from 'react';
 import LeaderBoard from '../../components/board/LeaderBoard';
 import Layout from '../Layout';
 
 function LeaderBoardPage() {
-  const [rankList, setRankList] = React.useState([]);
-
-  useEffect(() => {
-    (async () => {
-      const response = await axios(`${process.env.REACT_APP_API_DEFAULTS_URL}/user/rank`);
-      const { data } = response;
-      setRankList(data);
-    })();
-  }, []);
-
-  if (!rankList?.length) {
-    return <></>;
-  }
   return (
-    <div style={{ width: '90%', height: 'inherit', margin: 'auto' }}>
-      <Layout TopLeft={<LeaderBoard title="랭킹" data={rankList} />} />
+    <div>
+      <Layout TopLeft={<LeaderBoard />} />
     </div>
   );
 }
