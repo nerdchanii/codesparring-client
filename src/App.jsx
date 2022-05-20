9f36648d7e69c8f24bf700bd11e723be3f8ccc7f
nerdchanii <nerdchanii@gmail.com>
2022-05-20 23:42:21 +0900

    Remove: testcode that not need

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,20 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import Topper from './Pages/Layout/Topper';
 import RouteContainer from './Pages/Layout/RouteContainer';
 import './App.scss';
-import socket, { ANONY } from './constants/socket/socket';
-import { fetchLogin, logout } from './redux/reducers/auth.reducer';
-import { fetchSomething } from './redux/reducers/user.reducer';
 
 function App() {
-  const userLoggined = useSelector((state) => state.auth.isLoggedIn);
-  const dispatch = useDispatch();
-
-  const onClick = () => {
-    if (userLoggined) dispatch(logout());
-    else dispatch(fetchLogin({ email: ANONY, password: ANONY }));
-  };
-  const userClick = ()=>{
-    dispatch(fetchSomething());
-  }
   useEffect(() => {
-    socket.auth = { token: localStorage.getItem('LOGIN_TOKEN') || { nickName: ANONY } };
-    socket.connect();
+    // socket.auth = { token: localStorage.getItem('LOGIN_TOKEN') || { username: ANONY } };
+    // socket.connect();
+    // socket.on('connect', () => {
+    //   console.log('connected');
+    // });
+    // socket.emit('message', { message: 'Hello from client' });
   }, []);
   return (
     <div className="App">
       <Topper />
-      <button onClick={onClick}>{userLoggined ? '로그아웃' : '로그인'}</button>
-      <button onClick={userClick}>뭔가를 페치</button>
       <RouteContainer />
     </div>
   );
